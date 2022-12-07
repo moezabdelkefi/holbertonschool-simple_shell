68112952b1958c22a7acea9fe6e89affd4e17972
s4iTen <ychouk102@gmail.com>
2022-12-07 11:50:24 +0100

    update

--- simp_shell.h
+++ simp_shell.h
@@ -11,11 +11,16 @@
 #include <stdio.h>
 #include <string.h>
 
+
+
 int main(int ac, char **av, char **env);
 char **tokenization(char *ptr, char *delim);
 int comp_exec(char **tokens, char *ptr, char **env);
 int exec(char **tokens, char **env);
 void free_array(char **tokens);
 char *get_location(char* command);
+int _path(char *first, char **input, char **env, int *ex_st);
+void _execute(int status, char **args, int *ex_st, int *tal);
+void builtins(char *line, char **args, char **env, int *ex_st);
 
 #endif
