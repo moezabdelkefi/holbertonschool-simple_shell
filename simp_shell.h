149fa11b96ed94b96d119ff9efb01c6c770b1d55
s4iTen <ychouk102@gmail.com>
2022-12-07 12:06:04 +0100

    test

--- simp_shell.h
+++ simp_shell.h
@@ -11,20 +11,13 @@
 #include <stdio.h>
 #include <string.h>
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> refs/remotes/origin/main
 int main(int ac, char **av, char **env);
 char **tokenization(char *ptr, char *delim);
 int comp_exec(char **tokens, char *ptr, char **env);
 int exec(char **tokens, char **env);
 void free_array(char **tokens);
-<<<<<<< HEAD
 char *get_location(char* command);
 
-=======
-char *get_location(char *command);
->>>>>>> refs/remotes/origin/main
 #endif
